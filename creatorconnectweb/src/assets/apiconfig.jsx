fd33c1c3ce6d7eff77b460add02fbf2f27208e98
DoubleU <yuuliu.5533@gmail.com>
2023-12-13 03:50:08 -0500

    Everything is Editted

---
+++ creatorconnectweb/src/assets/apiconfig.jsx
@@ -0,0 +1,4 @@
+export const apiGetUserUrl = 'http://userappdev-env.eba-kbgrydns.us-east-1.elasticbeanstalk.com/user/self';
+export const apiLogoutUrl = 'http://userappdev-env.eba-kbgrydns.us-east-1.elasticbeanstalk.com/oauth/logout';
+export const apiUpdateSelfUrl = 'http://userappdev-env.eba-kbgrydns.us-east-1.elasticbeanstalk.com/user/self';
+export const apiUploadImage = 'http://userappdev-env.eba-kbgrydns.us-east-1.elasticbeanstalk.com/upload/image';
--- creatorconnectweb/src/components/Search.jsx
+++ creatorconnectweb/src/components/Search.jsx
@@ -1,19 +1,94 @@
 import '../index.css';
-import UserDisplay from './UserDisplay.jsx'; 
+import UserDisplay from './UserDisplay.jsx';
+import React, { useEffect, useState } from 'react';
 
-function Search() {
+function Search(props) {
+
+  const [search, setSearch] = useState("")
+  const [allusers, setAllUsers] = useState();
+
+  async function generateUsers() {
+    /// http://localhost:8080/user/generate?size=10
+
+    // list of users
+    const page = 0;
+    const limit = 10;
+    const name = ''; // `http://localhost:8080/user?page=${page}&limit=${limit}&name=whateveryournam`
+    const tags = '' // `http://localhost:8080/user?page=${page}&limit=${limit}&tags[]=suerpman,batman`
+
+    const listEnvelope = await fetch(`http://localhost:8080/user?page=${page}&limit=${limit}`, {
+      method: 'get',    
+      // mode: 'no-cors',
+      credentials: 'include'});
+      const list = await listEnvelope.json();   //Has all users
+    console.log('list users', list)
+
+    const myuserdata = list;
+    const filteredlist = [];  
+
+    list.map((foreignUserData) => {
+      console.log('search', search, foreignUserData.name, foreignUserData.name == search);
+      if(search == ""){
+        filteredlist.push(foreignUserData)
+      }
+      else{
+        if (foreignUserData.name == search){
+          filteredlist.push(foreignUserData)
+        }
+      }
+    })
+
+    console.log(filteredlist);
+    setAllUsers(filteredlist);
+  }
+
+  useEffect(() => {
+    generateUsers();
+    console.log(search)
+  },[search])
+
+  function generationHandler(text){
+    setSearch(text.target.value);
+    generateUsers(text.target.value);
+  }
+
   return (
     <div className="flex flex-col box-border h-full w-full pt-10 ">
       <div className="flex box-border h-[50px] w-full justify-center text-white text-2xl">
         Search:
         <div className="flex rounded-full bg-white h-11 w-5/6 border-double border-4 border-cyan-400 mx-5 px-5 text-black outline-none">
           <form className="w-full">
-            <input size="40" className="flex pr-[-500px] border-none w-[1000px] sm:w-80" placeholder="Search User . . ." />
+            <textarea type="text" onChange={generationHandler} size="40" class="border-hidden border-w-[0px] w-[100%] min-h-[35px] max-h-[35px] overflow-hidden" placeholder="Search User . . ." />
           </form>
         </div>
       </div>
 
-      <UserDisplay
+      {
+      (allusers || []).map((d) => { return(
+        <UserDisplay
+          username={d.name}
+          fullName={d.name}
+          profileImage={d.avatar}
+          subscriberCount={1000}
+          bio={d.bio}
+        />
+      )
+      })
+      }
+
+      {/* {
+      (allusers || []).map((d) => {
+        <UserDisplay
+          username={d.name}
+          fullName={d.name}
+          profileImage={d.avatar}
+          subscriberCount={1000}
+          bio={d.bio}
+        />
+      })
+      } */}
+
+      {/* <UserDisplay
         username="john_doe"
         fullName="John Doe"
         profileImage="/images/johndoe.png"
@@ -34,7 +109,7 @@ function Search() {
         profileImage="/images/johndoe.png"
         subscriberCount={69420}
         bio="Professional Call of Warfare eSports player rank #1 NA."
-      />
+      /> */}
     </div>
   );
   
