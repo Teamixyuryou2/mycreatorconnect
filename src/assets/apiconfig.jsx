c1885e3e39013ad38492008c47f1b2baf9fe3eb0
DoubleU <yuuliu.5533@gmail.com>
2023-12-13 07:15:29 -0500

    yoasd

--- src/assets/apiconfig.jsx
+++ src/assets/apiconfig.jsx
@@ -1,5 +1,5 @@
 
-const baseUrl = 'https://app.linkapphost.com';
+const baseUrl = 'https://69e6-68-175-9-36.ngrok-free.app';
 export const apiGetUserUrl = `${baseUrl}/user/self`
 export const apiLogoutUrl = `${baseUrl}/oauth/logout`
 export const apiUpdateSelfUrl = `${baseUrl}/user/self`
